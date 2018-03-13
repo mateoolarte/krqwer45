520e53ea6425c1387b1437b24273f180ef81e21e
Mateo Olarte <mateo.olarte8@gmail.com>
2018-03-13 01:21:56 -0500

    add task and mark done one task

--- src/App.js
+++ src/App.js
@@ -13,17 +13,50 @@ class App extends Component {
       ],
       newTask: ''
     }
+
+    this.updateValue = this.updateValue.bind(this) 
+    this.addTask = this.addTask.bind(this)
+    this.markDone = this.markDone.bind(this)
   }
+
+  updateValue(e) {
+    this.setState({
+      newTask: e.target.value
+    })
+  }
+
+  addTask(e) {
+    const objID = this.state.tasks.length + 1
+    let inputText = document.getElementById('new-task')
+
+    if (this.state.newTask.length !== 0) {
+      this.setState({
+        tasks: this.state.tasks.concat({id: objID, name: this.state.newTask, done: false}),
+        newTask: ""
+      })
+    } 
+
+    e.preventDefault()
+  }
+
+  markDone(e) {
+    this.setState({
+      tasks: this.state.tasks.map(task =>
+        e.target.id == task.id ? task.done ? { id: task.id, name: task.name, done: false } : { id: task.id, name: task.name, done: true } : task
+      )
+    })
+  }
+
   render() {
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            {this.state.tasks.map((task, index) => <li key={task.id}>{task.name}</li>)}
+            {this.state.tasks.map((task, index) => <li key={task.id} id={task.id} className={ task.done ? 'done' : '' } onClick={this.markDone}>{task.name}</li>)}
           </ul>
-          <form>
-            <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.newTask} />
+          <form onSubmit={this.addTask}>
+            <input type="text" id="new-task" className={this.state.newTask.length === 0 ? 'error' : ''} onChange={this.updateValue} placeholder="Ingresa una tarea y oprime Enter" value={this.state.newTask} />
           </form>
         </div>
       </div>
